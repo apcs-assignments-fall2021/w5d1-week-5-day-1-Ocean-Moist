00d87d23c241cc2808b7726f09ea7bf8d9e34698
Rohan <rohanzealous@gmail.com>
2021-09-28 09:55:56 -0700

    finishe

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -6,7 +6,17 @@ public class MyMain {
     // to sort our code
     // This algorithm returns the sorted array
     public static int[] selectionSort(int[] arr) {
-        // REPLACE THIS WITH YOUR CODE
+        for (int i = 0; i < arr.length-1; i++) {
+            int min = i;
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+            int tmp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = tmp;
+        }
         return arr;
     }
 
@@ -14,7 +24,16 @@ public class MyMain {
     // to sort our code
     // This algorithm returns the sorted array
     public static int[] insertionSort(int[] arr) {
-        // REPLACE THIS WITH YOUR CODE
+        for (int i = 1; i < arr.length; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
         return arr;
     }
 
